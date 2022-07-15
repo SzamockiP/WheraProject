ac50fe25329bd4ed34bc0609dccc60a14430954c
Piotr Szamocki <szamocki.piotr04@gmail.com>
2022-07-15 16:47:21 +0100

    Dodano czytanie tekstur oraz pliku z mapą

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import json
 
 pygame.init()
 
@@ -6,21 +7,25 @@ surface = pygame.display.set_mode((720, 480))
 
 pygame.display.set_caption("Testy")
 clock = pygame.time.Clock()
-basicTile = pygame.image.load("BasicTile.png")
 
+with open("mainMap.json", "r") as json_file:
+	data = json.load(json_file)
+	TILES = [pygame.image.load(data['tiles_to_load'][x]) for x in data['tiles_to_load']]
+	TILESET = data['tileset']
+	
+exit = False	
 
-exit = False
-height = 0
 while not exit:
 	surface.fill([0,0,0])
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			exit = True
 	
-	for x in range(20):
-		for y in range(20):
-			surface.blit(basicTile, (x*16 + y*-16 + 320, x*8 + y*8))  # martix (0.5x + -0.5y, 0.25x + 0.25y)
-		
+	for tile in TILESET:
+		pos_x = tile['pos_x']
+		pos_y = tile['pos_y']
+		surface.blit(TILES[tile['texture']], (pos_x*16 + pos_y*-16 + 320, pos_x*8 + pos_y*8 - tile['height'] * 10 + 150))
+	
 	pygame.display.update()
 
 	clock.tick(30)
